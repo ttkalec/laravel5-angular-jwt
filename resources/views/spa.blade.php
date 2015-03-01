004074f4988021aa478d80498ad70f77b1d2cf5d
Tino Tkalec <ttkalec1@gmail.com>
2015-03-01 11:06:44 +0100

    Change the title

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>JWT Laravel - AngularJS example</title>
+    <title>Laravel 5 / AngularJS JWT example</title>
 
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/bootstrap.superhero.min.css">
