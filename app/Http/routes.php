0abe6e10c963e20e29ae912add6a1103e76405f8
Tino Tkalec <ttkalec1@gmail.com>
2015-03-01 10:54:28 +0100

    Update code comments

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,49 +3,69 @@
 use App\User;
 use Illuminate\Http\Response as HttpResponse;
 
+/**
+ * Displays Angular SPA application
+ */
 Route::get('/', function () {
     return view('spa');
 });
 
+/**
+ * Registers a new user and returns a auth token
+ */
 Route::post('/signup', function () {
     $credentials = Input::only('email', 'password');
     $user = User::create($credentials);
 
-    $token = JWTAuth::fromUser($user);
+    try {
+        $token = JWTAuth::fromUser($user);
+    } catch (Exception $e) {
+        return Response::json(['error' => 'User already exists.'], HttpResponse::HTTP_CONFLICT);
+    }
+
     return Response::json(compact('token'));
 });
 
+/**
+ * Signs in a user using JWT
+ */
 Route::post('/signin', function () {
     $credentials = Input::only('email', 'password');
 
-    if ( ! $token = JWTAuth::attempt($credentials) )
-    {
+    if (!$token = JWTAuth::attempt($credentials)) {
         return Response::json(false, HttpResponse::HTTP_UNAUTHORIZED);
     }
 
     return Response::json(compact('token'));
 });
 
-Route::get('/restricted', ['before' => 'jwt-auth', function () {
-    $token = JWTAuth::getToken();
-    $user = JWTAuth::toUser($token);
 
-    return Response::json(['data' => [
-        'email' => $user->email,
-        'registered_at' => $user->created_at->toDateTimeString()
-    ]]);
-}]);
+/**
+ * Fetches a restricted resource from the same domain used for user authentication
+ */
+Route::get('/restricted', [
+    'before' => 'jwt-auth',
+    function () {
+        $token = JWTAuth::getToken();
+        $user = JWTAuth::toUser($token);
 
-Route::group(['domain' => 'api.jwt.dev', 'prefix' => 'v1'], function()
-{
-    Route::get('/restricted', function()
-    {
-        try
-        {
-            $user = JWTAuth::parseToken()->toUser();
-        }
-        catch(Exception $e)
-        {
+        return Response::json([
+            'data' => [
+                'email' => $user->email,
+                'registered_at' => $user->created_at->toDateTimeString()
+            ]
+        ]);
+    }
+]);
+
+/**
+ * Fetches a restricted resource from API subdomain using CORS
+ */
+Route::group(['domain' => 'api.jwt.dev', 'prefix' => 'v1'], function () {
+    Route::get('/restricted', function () {
+        try {
+            JWTAuth::parseToken()->toUser();
+        } catch (Exception $e) {
             return Response::json(['error' => $e->getMessage()], HttpResponse::HTTP_UNAUTHORIZED);
         }
 
